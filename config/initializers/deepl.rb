400f9cb209429316865feceac30952bb03cf007e
Alexander Rusa <alex@rusa.at>
2022-12-14 21:58:06 +0000

    Replace deepl with aws

--- config/initializers/deepl.rb
+++ config/initializers/deepl.rb
@@ -3,9 +3,9 @@
 # DeepL Translation service configuration
 # you can also set the api key using an env var:
 #  export DEEPL_AUTH_KEY="your-api-token"
-if Rails.env == "production"
-  DeepL.configure do |config|
-    config.auth_key = ENV['DEEPL_AUTH_KEY'] || Rails.application.credentials.dig(:translator, :api_key)
-    config.host = ENV['DEEPL_HOST'] || Rails.application.credentials.dig(:translator, :host)
-  end
-end
+# if Rails.env == "production"
+#   DeepL.configure do |config|
+#     config.auth_key = ENV['DEEPL_AUTH_KEY'] || Rails.application.credentials.dig(:translator, :api_key)
+#     config.host = ENV['DEEPL_HOST'] || Rails.application.credentials.dig(:translator, :host)
+#   end
+# end
