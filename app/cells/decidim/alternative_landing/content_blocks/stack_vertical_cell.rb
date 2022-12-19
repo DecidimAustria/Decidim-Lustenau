e1b384d6ed3c7791f90a04931c1c24283d1cdc2a
Alex <alex@rusa.at>
2022-12-19 18:41:17 +0000

    Add cell from gem

---
+++ app/cells/decidim/alternative_landing/content_blocks/stack_vertical_cell.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Decidim
+  module AlternativeLanding
+    module ContentBlocks
+      class StackVerticalCell < BaseCell
+        def translated_title
+          translated_attribute(model.settings.title)
+        end
+
+        def translated_body(item_number)
+          translated_attribute(model.settings.send("body_#{item_number}"))
+        end
+
+        def translated_link(item_number, classes: "")
+          link_to translated_attribute(model.settings.send("link_text_#{item_number}")), translated_attribute(model.settings.send("link_url_#{item_number}")), class: classes
+        end
+
+        def translated_tag(item_number, classes: "")
+          link_to translated_attribute(model.settings.send("tag_text_#{item_number}")), translated_attribute(model.settings.send("tag_url_#{item_number}")), class: classes
+        end
+
+        def image(item_number)
+          model.images_container.send("image_#{item_number}").variant(resize_to_fill: [960, 960])
+        end
+      end
+    end
+  end
+end
