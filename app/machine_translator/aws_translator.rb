614d9b988d5aed426922710b637df0267fdcda8c
Alex <alex@rusa.at>
2023-01-16 08:14:25 +0000

    Fix sentry debugging

--- app/machine_translator/aws_translator.rb
+++ app/machine_translator/aws_translator.rb
@@ -17,7 +17,19 @@ class AwsTranslator
   end
 
   def translate
-    Sentry.capture_message "Running translate with text: #{@text.truncate(30)} (#{@text.bytesize}), source: #{@source_locale}, target: #{@target_locale} for field #{@field_name} of resource #{@resource}"
+    Sentry.with_scope do |scope|
+      scope.set_context(
+        'translation',
+        {
+          text: @text,
+          source_locale: @source_locale,
+          target_locale: @target_locale,
+          field_name: @field_name,
+          resource: @resource
+        }
+      )
+      Sentry.capture_message("Running translate")
+    end
     return if @text.blank?
 
     # remove base64 encoded images if they exist
@@ -26,7 +38,23 @@ class AwsTranslator
 
     translation = aws_translate
 
-    Sentry.capture_message "Creating MachineTranslationSaveJob with translated text: #{translation.translated_text.truncate(30)} (#{translation.translated_text.bytesize}), source: #{@source_locale}, target_locale: #{target_locale}, @target_locale: #{@target_locale}, field_name: #{field_name}, resource: #{resource}, @field_name: #{@field_name}, @resource: #{@resource}"
+    Sentry.with_scope do |scope|
+      scope.set_context(
+        'translation',
+        {
+          text: @text,
+          source_locale: source_locale,
+          target_locale: target_locale,
+          field_name: field_name,
+          resource: resource,
+          at_source_locale: @source_locale,
+          at_target_locale: @target_locale,
+          at_field_name: @field_name,
+          at_resource: @resource
+        }
+      )
+      Sentry.capture_message("Creating MachineTranslationSaveJob with translated text")
+    end
     Decidim::MachineTranslationSaveJob.perform_later(
       @resource,
       @field_name,
@@ -36,7 +64,18 @@ class AwsTranslator
   end
 
   def aws_translate
-    Sentry.capture_message "Contacting AWS Translate service with text: #{@text.truncate(30)} (#{@text.bytesize}), source: #{@source_locale}, target: #{@target_locale}"
+    Sentry.with_scope do |scope|
+      scope.set_context(
+        'translation',
+        {
+          text: @text,
+          source_locale: @source_locale,
+          target_locale: @target_locale
+        }
+      )
+      Sentry.capture_message("Contacting AWS Translate") # will be tagged with `my_tag: "my value"`
+    end
+
     aws_client = Aws::Translate::Client.new(region: @region, credentials: @credentials)
     aws_client.translate_text(
       text: @text, # required
