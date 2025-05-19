c4c51e1e18f4d086df9b119697595516fcc7d52a
webchroma <info@webchroma.de>
2025-05-19 09:02:45 +0000

    gitpod

--- connect_local_db.sh
+++ connect_local_db.sh
@@ -9,8 +9,8 @@ export DATABASE_URL=postgresql://gitpod@localhost
 export PGHOSTADDR=127.0.0.1
 export DECIDIM_HOST="3000-$GITPOD_WORKSPACE_ID.$GITPOD_WORKSPACE_CLUSTER_HOST"
 export DECIDIM_FOLLOW_HTTP_X_FORWARDED_HOST=true
-# export DECIDIM_FORCE_SSL=false
-# export DECIDIM_ALLOW_OPEN_REDIRECTS=true
+export DECIDIM_FORCE_SSL=false
+export DECIDIM_ALLOW_OPEN_REDIRECTS=true
 
 unset DATABASE_HOST
 unset DATABASE_PORT
@@ -26,4 +26,4 @@ echo "update remote organization hostname"
 echo "UPDATE decidim_organizations SET host='3000-$GITPOD_WORKSPACE_ID.$GITPOD_WORKSPACE_CLUSTER_HOST';" | bundle exec rails dbconsole -p
 echo 'Decidim::User.first.update(accepted_tos_version: DateTime.now, admin_terms_accepted_at: DateTime.now, password_updated_at: DateTime.now)' | bundle exec rails c
 
-bin/rails server
+bin/dev
