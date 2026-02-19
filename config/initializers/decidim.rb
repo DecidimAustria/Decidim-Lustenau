0d9e30b458d65f64e877a4d4972d44d5da0368f1
Alexander Rusa <alex@rusa.at>
2026-02-19 22:26:14 +0100

    Add matomo to csp

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -351,6 +351,7 @@ Decidim.configure do |config|
     "img-src": %w(https://cdn.participation.works),
     "connect-src": %w(https://cdn.participation.works),
     "frame-src": %w(www.youtube.com),
+    "script-src": %w(https://ma.participation.works)
   }
 
   # Defines data consent categories and the data stored in each category.
